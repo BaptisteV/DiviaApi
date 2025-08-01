066372995279ec048355b5c739316f2ce1f920fd
Baptiste Viloin <baptiste.viloin.iris@gmail.com>
2025-08-02 00:27:20 +0200

    Deploy script update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 git pull origin master
 docker stop $(docker ps -a -q)
 docker build -t morningapi .
-docker run -d -p 192.168.1.132:80:8080 morningapi
+docker run -d -p 192.168.1.132:8080:8080 morningapi
